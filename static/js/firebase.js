466f344a7d3fa68a44f8f2c8eec4c50a098f462e
Renzo <rbenemerito1@gmail.com>
2018-11-17 13:10:12 +0800

    Connect to Firebase

--- static/js/firebase.js
+++ static/js/firebase.js
@@ -9,156 +9,125 @@
     };
     firebase.initializeApp(config);
     
-    var val = 0;
+    var val;
     var ref = firebase.database().ref().child("Dust Density");
 
     ref.on("value", function(snapshot) {
-        val = snapshot.val()*-100;
-
-        console.log(val);
+        val = snapshot.val()
+        console.log(val)
+        
         progress(val);
     }, function (error) {
     console.log("Error: " + error.code);
     });
-    var int = 0
-    setInterval(update, 1000);
-    function update(){ 
-      val+=Math.random() * (100+0) +0;
-      if(val > 100){
-        val-=100;
-      } 
-      progress(val);
-      console.log(val);
-      updateReport(val);
-    }
-    var myChart;
-    try {
 
-        // Recent Report 2
-        const bd_brandProduct2 = 'rgba(0,181,233,0.9)'
-        const bd_brandService2 = 'rgba(0,173,95,0.9)'
-        const brandProduct2 = 'rgba(0,181,233,0.2)'
-        const brandService2 = 'rgba(0,173,95,0.2)'
     
-        var data3 = []
-       
-    
-        var ctx = document.getElementById("recent-rep2-chart");
-        if (ctx) {
-          ctx.height = 230;
-          var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-              labels: [],
-              datasets: [
-                {
-                  label: 'Dust Level',
-                  backgroundColor: brandProduct2,
-                  borderColor: bd_brandProduct2,
-                  pointHoverBackgroundColor: '#fff',
-                  borderWidth: 0,
-                  data: data3
-    
+    function history(){
+        try {
+
+            // Recent Report 2
+            const bd_brandProduct2 = 'rgba(0,181,233,0.9)'
+            const bd_brandService2 = 'rgba(0,173,95,0.9)'
+            const brandProduct2 = 'rgba(0,181,233,0.2)'
+            const brandService2 = 'rgba(0,173,95,0.2)'
+        
+            var data3 = [52, 60, 55, 50, 65, 80, 57, 70, 105, 115]
+           
+        
+            var ctx = document.getElementById("recent-rep2-chart");
+            if (ctx) {
+              ctx.height = 230;
+              var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', ''],
+                  datasets: [
+                    {
+                      label: 'Dust Level',
+                      backgroundColor: brandProduct2,
+                      borderColor: bd_brandProduct2,
+                      pointHoverBackgroundColor: '#fff',
+                      borderWidth: 0,
+                      data: data3
+        
+                    },
+                    // {
+                    //   label: 'My Second dataset',
+                    //   backgroundColor: brandProduct2,
+                    //   borderColor: bd_brandProduct2,
+                    //   pointHoverBackgroundColor: '#fff',
+                    //   borderWidth: 0,
+                    //   data: data4
+        
+                    // }
+                  ]
                 },
-                // {
-                //   label: 'My Second dataset',
-                //   backgroundColor: brandProduct2,
-                //   borderColor: bd_brandProduct2,
-                //   pointHoverBackgroundColor: '#fff',
-                //   borderWidth: 0,
-                //   data: data4
-    
-                // }
-              ]
-            },
-            options: {
-              maintainAspectRatio: true,
-              legend: {
-                display: false
-              },
-              responsive: true,
-              scales: {
-                xAxes: [{
-                  gridLines: {
-                    drawOnChartArea: true,
-                    color: '#f2f2f2'
+                options: {
+                  maintainAspectRatio: true,
+                  legend: {
+                    display: false
                   },
-                  ticks: {
-                    fontFamily: "Poppins",
-                    fontSize: 12
-                  }
-                }],
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true,
-                    maxTicksLimit: 5,
-                    stepSize: 50,
-                    max: 150,
-                    fontFamily: "Poppins",
-                    fontSize: 12
+                  responsive: true,
+                  scales: {
+                    xAxes: [{
+                      gridLines: {
+                        drawOnChartArea: true,
+                        color: '#f2f2f2'
+                      },
+                      ticks: {
+                        fontFamily: "Poppins",
+                        fontSize: 12
+                      }
+                    }],
+                    yAxes: [{
+                      ticks: {
+                        beginAtZero: true,
+                        maxTicksLimit: 5,
+                        stepSize: 50,
+                        max: 150,
+                        fontFamily: "Poppins",
+                        fontSize: 12
+                      },
+                      gridLines: {
+                        display: true,
+                        color: '#f2f2f2'
+        
+                      }
+                    }]
                   },
-                  gridLines: {
-                    display: true,
-                    color: '#f2f2f2'
-    
+                  elements: {
+                    point: {
+                      radius: 0,
+                      hitRadius: 10,
+                      hoverRadius: 4,
+                      hoverBorderWidth: 3
+                    },
+                    line: {
+                      tension: 0
+                    }
                   }
-                }]
-              },
-              elements: {
-                point: {
-                  radius: 0,
-                  hitRadius: 10,
-                  hoverRadius: 4,
-                  hoverBorderWidth: 3
-                },
-                line: {
-                  tension: 0
+        
+        
                 }
-              }
-    
-    
+              });
             }
-          });
-        }
-    
-      } catch (error) {
-        console.log(error);
-      }
-  function updateReport(val){
-    var len = myChart.data.datasets[0].data.length
-    var d = new Date(); // for now
-    var h = d.getHours(); // => 9
-    var m = d.getMinutes(); // =>  30
-    var s = d.getSeconds(); // => 51
-    if(len>30){
-      myChart.data.datasets[0].data.shift();
-      myChart.data.labels.shift();
-      myChart.data.datasets[0].data[len-1] = val;
-      myChart.data.labels[len-1] = h+":"+m+":"+s;
-      myChart.update();
-    }
-    else{
-      myChart.data.datasets[0].data[len] = val;
-      myChart.data.labels[len] = h+":"+m+":"+s;
-      myChart.update();
+        
+          } catch (error) {
+            console.log(error);
+          }
     }
     
-  }
       
-  
 
 //Dust Level
-  function progress(val){
+   function progress(val){
     try {
-        if(val > 100){
-          val = 100;
-        }
         var progressbarSimple = $('.js-progressbar-simple');
         progressbarSimple.each(function () {
           var that = $(this);
           var executed = false;
          
-          $("#dustLevel").attr("data-transitiongoal", val);
+          $("#dustLevel").attr("data-transitiongoal", val*-100);
             that.waypoint(function () {
               if (!executed) {
                 executed = true;
